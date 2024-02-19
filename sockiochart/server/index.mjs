289fd54a62a6a4a77e871d475e1d0e5c36bd060e
Adem <adem5434e@gmail.com>
2024-02-19 02:05:18 +0100

    update

--- sockiochart/server/index.mjs
+++ sockiochart/server/index.mjs
@@ -39,7 +39,6 @@ io.on("connection", (socket)=>{
     if (data.length > 5) {
       data.shift(); // Remove the first element
     }
-
     // Add a new data point
     data.push({
       name: (parseInt(data[data.length - 1].name) + 1).toString(),
@@ -64,6 +63,6 @@ httpServer.listen("5002");
 
     //timeChange = setInterval(()=> socket.emit("message", new Date()),1000)
 //Her 1 saniyede bize yeni data geldgini dusunelim, simule edelim...ondan dolayi da biz bu is icin new Date() i kullandik ki surekli degistigi icin bizim amacimza uygun islem yapmak icin. Ama gercek hayatta her 1 dakika da veya 30 saniye de 1 bize guncel data donenen backend service leri vardir ornegin iste bu tarz servislerden alaagiz biz gercek hayatta data yi backendde ve socket-server a aktaracagiz ordan..ve socket-server dand a tabi ki clienta her 30 saniye, 1 dakika , 2 saniye  , 1 saniye de bir data yi client tarafinda da guncelleyebilmi solacagiz
-//Her 1 saniyede birkez mesaj gonderecek
+//Her 1 saniyede birkez mesaj gonderecek    
 
 //!HARIKA BESTPRACTISE...ISTE ANLIK OLARAK DEGISTIRILEN DATALARIN HEPSINDEKI MANTIK BUDUR...YANI SERVER TARAFNDA-BACKENDDE KULLANIALN BIR SERVICE DEN GELEN BELLI ARALIKLARALA GELEN ONLINE BIR DATA ALINIP SOKCET-IO YA AKTARILIYOR VE SOCKET-IO DA BUNU AYNI ARALIKLARLA CLIENTA AKTARARAK ON YUZDE REAL TIME BIR SEKILDE DATA LARIN TAKIP EDILMESI SAGLANMIS OLUYOR....
